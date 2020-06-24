a4927e1fc784f825399f2a45eba9e976581400cf
First Last <john.miller30@gmail.com>
2020-06-24 14:53:42 -0600

    completed taks 5

--- index.js
+++ index.js
@@ -54,7 +54,14 @@ console.log(monthlyRate)
 
 If your name is `Oscar` mortgageCalculator() should return "Oscar, your monthly rate is 1073.64"
 */
-function mortgageCalculator(principal, interestrate, years){
+function mortgageCalculator(principal, interestrate, years,creditScore){
+    if (creditScore>740){
+        interestrate=interestrate*0.95;
+
+    }
+    else if(creditScore<660){
+            interestrate=interestrate*1.05;
+    }
     var monthlyInterestRate=interestrate/12;
     var periods=years*12;
     var n1=Math.pow(1+monthlyInterestRate,periods);
@@ -65,7 +72,7 @@ function mortgageCalculator(principal, interestrate, years){
 
 }
 
-
+console.log(mortgageCalculator(200000, 0.05, 30, 540))
 
 
 
@@ -76,18 +83,20 @@ For example,
 mortgageCalculator(200000, 0.05, 30); <-- should return 1,073.64
 */
 
-
+//completed this on task 3
 
 
 
 // 🏡 Task 5: Conditionals
 /* Add another paramter to your function called credit score. This parameter will be a number between 0 and 800 (a credit score).
 
-Then, add control flow within your function such that IF creditScore is above 740, interest rate drops by 0.5%, if credit score is below 660, interest rate increases by 0.5% and if credit score is anywhere between 660 and 740 interest rate doesn't change.
+Then, add control flow within your function such that IF creditScore is above 740, interest rate drops by 0.5%, 
+if credit score is below 660, interest rate increases by 0.5% and if credit score is anywhere between 660 and 740 
+interest rate doesn't change.
 
 Hint: To drop an interest rate by 5% you can take monthlyRate and multiply it by 0.95. Similarly, to increase an interest rate by 5% you'd do monthlyRate * 1.05. 
 */
-
+//doing work on function on lines 57
 
 
 
